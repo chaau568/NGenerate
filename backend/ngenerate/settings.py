a196e948de4415ba916dc221bae743126fb7f6f3
chaau568 <chaiyapatpolyiam@gmail.com>
2026-02-23 12:24:30 +0700

    config cors

--- backend/ngenerate/settings.py
+++ backend/ngenerate/settings.py
@@ -216,8 +216,8 @@ TTS_SERVICE_URL = env("TTS_SERVICE_URL", default='https://runpod-xxxxx.proxy.run
 # COSR Config
 # -------------------------------------------------
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
